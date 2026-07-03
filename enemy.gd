4aa0a9fdf497fd052e04dd08dbfe0003013b4464
Lauri Kumpulainen <late.kumpulainen@gmail.com>
2026-07-03 08:41:19 +0300

    made enemy chase player

--- enemy.gd
+++ enemy.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-
+const SPEED = 250.0
+var target_position
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var angular_speed = PI
-	rotation += angular_speed * delta # make the enemy turn in place
-	var velocity = Vector2.UP.rotated(rotation) * SPEED # make the enemy velocity point up perpendicular of the rotation
-	position += velocity * delta # make the enemy move forward based on velocity
-	move_and_slide()
+	#var angular_speed = PI
+	#rotation += angular_speed * delta # make the enemy turn in place
+	# velocity = Vector2.UP.rotated(rotation) * SPEED # make the enemy velocity point up perpendicular of the rotation
+	# position += velocity * delta # make the enemy move forward based on velocity
+	# move_and_slide()
+	pass
+
+
+func _on_player_existed(player_position: Variant) -> void:
+	target_position = (player_position - position).normalized()
+	if position.distance_to(player_position) > 3:
+		velocity = target_position*SPEED
+		move_and_slide()
