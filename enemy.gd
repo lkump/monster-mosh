10392a67b795aac00e0c53782c51ffdf31fff978
Lauri Kumpulainen <late.kumpulainen@gmail.com>
2026-07-03 18:26:13 +0300

    player moved to center of screen

--- enemy.gd
+++ enemy.gd
@@ -5,13 +5,6 @@ const SPEED = 150.0
 var target_position
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var angular_speed = PI
-	#rotation += angular_speed * delta # make the enemy turn in place
-	# velocity = Vector2.UP.rotated(rotation) * SPEED # make the enemy velocity point up perpendicular of the rotation
-	# position += velocity * delta # make the enemy move forward based on velocity
-	# move_and_slide()
 	pass
 
 
