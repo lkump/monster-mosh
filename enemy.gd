fe5cb08613ec43e112cddf8b7c0e2c8e72e8bc0f
Lauri Kumpulainen <late.kumpulainen@gmail.com>
2026-07-03 07:59:51 +0300

    made enemy move in a circle

--- enemy.gd
+++ enemy.gd
@@ -8,6 +8,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var angular_speed = PI
-	rotation += angular_speed * delta
-
+	rotation += angular_speed * delta # make the enemy turn in place
+	var velocity = Vector2.UP.rotated(rotation) * SPEED # make the enemy velocity point up perpendicular of the rotation
+	position += velocity * delta # make the enemy move forward based on velocity
 	move_and_slide()
