63c0822525dcc60a1bd02da546d2a281589dab04
Lauri Kumpulainen <late.kumpulainen@gmail.com>
2026-07-09 15:18:41 +0300

    player stat changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,42 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+@export var stats : PlayerStatisticsResource
+var speed
 signal existed(player_position)
+var attack_damage 
+var health 
 
-
+func _ready() -> void:
+	speed = stats.speed
+	health = stats.health
+	attack_damage = stats.attack_damage
+	
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var x_direction := Input.get_axis("left", "right")
 	if x_direction:
-		velocity.x = x_direction * SPEED
+		velocity.x = x_direction * speed
 		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 	var y_direction := Input.get_axis("up","down")
 	if y_direction:
-		velocity.y = y_direction * SPEED
+		velocity.y = y_direction * speed
 	else: 
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, speed)
 	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
+		velocity = velocity.normalized() * speed
 		
 	move_and_slide()
 	existed.emit(position)
 	
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("attack"):
-		print("attacked")
+		print("attacked with " + str(attack_damage) + " damage")
 	
+
+func _on_hurt_area_2d_took_damage(amount: int) -> void:
+	health = health-amount
+	print(health)
